1ea22a1e4b603c3643efe1a379f945c8326a527c
Sufu.Wang <1137120948@qq.com>
2025-10-16 22:45:14 +0800

    feat: set color of sticky column shadow

--- src/components/base-table.tsx
+++ src/components/base-table.tsx
@@ -80,7 +80,7 @@ export default function BaseTable<T>({ fixedColumnCount = -1, data, columns, }: 
     if (isScroll && fixedColumnCount > 0 && scrollLeft > 0 && fixedColumnCount -1 >= index) {
       const tableRowChildNodes = tableRowRef.current!.children
       const cls = new Array(fixedColumnCount).fill(`sticky bg-background z-10 ${defaultClassName}`)
-      cls[fixedColumnCount - 1] = `${cls.at(-1)} after:w-[30px] after:h-full after:absolute after:top-0 after:right-[-30px] after:shadow-[inset_8px_0_6px_-6px_rgba(0,0,0,0.15)]`
+      cls[fixedColumnCount - 1] = `${cls.at(-1)} after:w-[30px] after:h-full after:absolute after:top-0 after:right-[-30px] after:shadow-[inset_8px_0_6px_-6px_rgba(var(--shadow))]`
       return {
         className: cls[index],
         style: {
