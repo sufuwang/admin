af12046b8c1a45be472ca4092ca8cd54432831e0
sufuwang <1137120948@qq.com>
2025-10-25 15:41:46 +0800

    fix: get defaultLegendsValue from state

--- src/features/asserts/comps/outcome/line.tsx
+++ src/features/asserts/comps/outcome/line.tsx
@@ -70,7 +70,7 @@ export default function Line() {
           <ChartLegend
             content={
               <ChartLegendContent
-                defaultLegendsValue={defaultLegends}
+                defaultLegendsValue={legends}
                 onClickLegend={onClickLegend}
               />
             }
